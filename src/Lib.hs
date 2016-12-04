5ae24038d90b8bf2a20c534811189f446abcb055
kusut <tinokusut@gmail.com>
2016-12-04 19:22:18 +0700

    add single employee endpoint

--- src/Lib.hs
+++ src/Lib.hs
@@ -19,17 +19,23 @@ $(deriveJSON defaultOptions ''Employee)
 
 
 
-
 type API = "employees" :> Get '[JSON] [Employee]
+      :<|> "employees" :> Capture "userId" Int :> Get '[JSON] Employee
 
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return employees
 
-employees :: [Employee]
-employees = [Employee 1 "Fripp"]
+employeeHandler :: Int -> Handler Employee
+employeeHandler userId = return $ Employee userId "NO U"
+
+employeesHandler :: Handler [Employee]
+employeesHandler = return [Employee 1 "Fripp"]
+
+
+server :: Server API
+server = employeesHandler
+    :<|> employeeHandler
 
 
 app :: Application
